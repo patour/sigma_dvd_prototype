209b6cc0110b729a3d5c100439e828a22a1d6a74
Patrick Rasetarinera Rabarison <patrick.rasetarinera-rabarison@siemens.com>
2026-01-10 16:15:22 -0800

    Refactor run_pdn_parser.sh to improve documentation and parameter handling

--- run_pdn_parser.sh
+++ run_pdn_parser.sh
@@ -1,1 +1,22 @@
-python ./pdn/pdn_parser.py --netlist-dir ./netlist_data --net VDD --output ./netlist_data/pdn_graph.pkl --verbose
+#!/bin/bash
+
+# Parse PDN netlist and generate pickle graph
+# 
+# Usage:
+#   ./run_pdn_parser.sh                                    # Uses defaults
+#   ./run_pdn_parser.sh ./pdn/netlist_test                # Custom netlist dir
+#   ./run_pdn_parser.sh ./pdn/netlist_test VSS            # Custom dir and net name
+#
+# Parameters:
+#   $1 - Netlist directory (default: ./netlist_data)
+#   $2 - Power net name (default: VDD)
+#
+# Output:
+#   <netlist_dir>/pdn_graph.pkl - Serialized NetworkX graph
+
+# Default parameters
+NETLIST_DIR="${1:-./ netlist_data}"
+NET_NAME="${2:-VDD}"
+OUTPUT="${NETLIST_DIR}/pdn_graph.pkl"
+
+python ./pdn/pdn_parser.py --netlist-dir "$NETLIST_DIR" --net "$NET_NAME" --output "$OUTPUT" --verbose
